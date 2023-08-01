899f7323a29b179b416c6dea34d0762e4c9d5161
Debotos Das <debotosdas@gmail.com>
2023-08-01 07:41:21 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Personal on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
